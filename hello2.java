357cd362e18bdb01e670e407c2b9918df593c9fd
pcherugh <pcherugh@LIN66006118.corp.capgemini.com>
2020-05-22 22:31:01 +0530

    1030

--- hello2.java
+++ hello2.java
@@ -1,5 +1,5 @@
 class hello2{  
     public static void main(String args[]){  
-     System.out.println("modified in Eclips 7.13, This is hello2 2nd file/ Job on. 3rd job follows!!");  
+     System.out.println("modified in Eclips 10.30, This is hello2 2nd file/ Job on. 3rd job follows!!");  
     }  
 } 
