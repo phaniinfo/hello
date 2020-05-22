b14b75c3814b6f156180bb58bdfd2af9d7694fc1
pcherugh <pcherugh@LIN66006118.corp.capgemini.com>
2020-05-22 18:09:39 +0530

    modified in Eclipse

---
+++ HelloWorld1/newtest.java
@@ -0,0 +1,5 @@
+class newtest{  
+    public static void main(String args[]){  
+     System.out.println("New test file, from Eclipse 22May 4.56pm");  
+    }  
+} 
--- hello2.java
+++ hello2.java
@@ -1,5 +1,5 @@
 class hello2{  
     public static void main(String args[]){  
-     System.out.println("This is 2nd file/ Job on. 3rd job follows!!");  
+     System.out.println("modified in Eclips, This is hello2 2nd file/ Job on. 3rd job follows!!");  
     }  
 } 
